0830e6903937fe32065364e667503d053aae0668
rehana9600 <1713758438@qq.com>
2018-12-10 08:57:55 +0800

    李姣姣 添加了caseAnswer页面，csae添加了maskp

--- ScoutUI_Two/src/main.js
+++ ScoutUI_Two/src/main.js
@@ -3,11 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
- import "jquery"
 
 import "jquery"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap"
+
 import "./assets/css/base.css"
 
 import Swiper from 'swiper'
